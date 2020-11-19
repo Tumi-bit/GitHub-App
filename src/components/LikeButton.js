061eff855df97c2a47400e16b40da207b42b1ea3
Itumeleng Kekane <kekane.i96@gmail.com>
2020-11-19 14:50:15 +0100

    add LikeButton file

---
+++ src/components/LikeButton.js
@@ -0,0 +1,46 @@
+{/*import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import { AiOutlineStar } from 'react-icons/ai'
+
+function LikeButton() {
+  const isStarred = false;
+  return (
+    <div>
+  <button onClick={onStarClicked}>
+      {isStarred ? <AiFillStar></AiFillStar> : <AiOutlineStar></AiOutlineStar>} 
+    </button>
+    </div>
+  );
+}
+export default LikeButton 
+
+*class LikeButton extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        liked: false
+      };
+      this.handleClick = this.handleClick.bind(this);
+    } 
+    
+    handleClick() {
+      this.setState({
+        liked: !this.state.liked
+      });
+    }
+    
+    render() {
+     
+      return (
+        <div>
+          <button className="btn btn-primary" onClick={this.handleClick}>
+              {this.state.liked ? <AiFillStar></AiFillStar>: <AiOutlineStar></AiOutlineStar>}
+            </button>
+        </div>
+      );
+    }
+  }*/}
+
+ 
+ 
+  
